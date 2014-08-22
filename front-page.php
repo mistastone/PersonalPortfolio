369c25a4cd9e999cb3da197289e2247e77ec0814
mistastone <michael.stone2@gmail.com>
2014-08-22 11:47:39 -0400

    Added some style

--- front-page.php
+++ front-page.php
@@ -1,20 +1,87 @@
 <?php get_header(); ?>
 
+<!-- Main Background Image and Header -->
 	<div class="mainbackground">
 		<img src="wp-content/themes/theme-hackeryou/assets/images/background.jpg" alt="Main background image">
-		<div class="ribbon">
-	      <h1>@by<span>Michael</span>Stone</h1>
-	  </div>
 	</div>
 	
+	<div class="ribbon">
+	    <h1>@by<span>Michael</span>Stone</h1>
+	</div>
+
+<!-- Name Ribbon -->
 	<div class="devribbon">
 		<h1>Web Developer</h1>
 	</div>
 
+<!-- About Section -->
+	<section class = "aboutsection">
+		<div class = "aboutribbon">
+			<h1>About</h1>
+		</div>
 
-<div class="portfoliosection">
-	<h1>Portfolio</h1>
+		<div class="writeup">
 
+			<div class="aboutcontent clearfix">
+				<img src="wp-content/themes/theme-hackeryou/assets/images/michael.jpg" alt="Michael">
+				<h3>About</h3>
+				<hr>
+				<p>Hello. I am a front-end web developer and writer from Toronto. Hello. I am a front-end web developer and writer from Toronto.Hello. I am a front-end web developer and writer from Toronto.Hello. I am a front-end web developer and writer from Toronto.Hello. I am a front-end web developer and writer from Toronto.Hello. I am a front-end web developer and writer from Toronto.</p>
+
+				<h3>Skills</h3>
+				<hr>
+				<ul>
+					<li>Proficient in HTML5, CSS3, and SASS</li>
+					<li>Comfortable working with JavaScript and jQuery</li>
+					<li>Built apps using AJAX and various APIs</li>
+					<li>Comfortable building fully functional WordPress sites and using the WordPress back-end</li>
+					<li>Being a writer also enables me to write excellent content</li>
+				</ul>
+
+				</div>
+			</div>
+	</section>
+
+<!-- Skills Section -->
+	<!-- <section class = "skills">
+		<div class="aboutribbon">
+			<h1>Skills</h1>
+		</div>
+		
+
+			<div class="skillcontent">
+
+				<div class="box">
+					<img src="wp-content/themes/theme-hackeryou/assets/images/html.png" alt="HTML">
+					<p>HTML5 and CSS3</p>
+				</div>
+
+				<div class="box">
+					<img src="wp-content/themes/theme-hackeryou/assets/images/javascript.png" alt="JavaScript">
+					<p>JavaScript and jQuery</p>
+				</div>
+
+				<div class="box">
+					<img src="wp-content/themes/theme-hackeryou/assets/images/wordpress.png" alt="WordPress">
+					<p>WordPress</p>
+				</div>
+
+				<div class="box">
+					<img src="wp-content/themes/theme-hackeryou/assets/images/writer.png" alt="Content Writer">
+					<p>Content Writing</p>
+				</div>
+
+		
+		</div>
+	</section> -->
+
+<!-- Portfolio Section -->
+	<section class="portfoliosection">
+		<div class="aboutribbon">
+			<h1>Portfolio</h1>
+		</div>
+
+		<div class="portup clearfix">
 <?php
 
 $onePageQuery = new WP_Query( 
@@ -34,7 +101,7 @@ $onePageQuery = new WP_Query(
 		<?php while( has_sub_field('images') ): ?>
             
                 <?php $image = get_sub_field('image'); ?>
-                <img src="<?php echo $image['sizes']['medium'] ?>">
+                <img src="<?php echo $image['sizes']['large'] ?>">
             <?php endwhile; ?>
 			<h2><?php the_title(); ?></h2>
 			<?php the_content(); ?>
@@ -47,12 +114,17 @@ $onePageQuery = new WP_Query(
 	
 	<?php wp_reset_postdata(); ?>
 
-
-</div>
+	</div>
+</section>
 	
 <?php else:  ?>
 	[stuff that happens if there aren't any posts]
 <?php endif; ?>
 
+	<section class = "contact">
+		<div class="aboutribbon">
+			<h1>Contact</h1>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
