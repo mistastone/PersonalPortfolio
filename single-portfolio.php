ed39369893ac3b5b74445e8ec7ab439d1a5dd1d7
mistastone <michael.stone2@gmail.com>
2014-08-26 00:02:51 -0400

    Styled blog pages and portfolio pages. Added View Online link to portfolio pages. Did Drive site write up

--- single-portfolio.php
+++ single-portfolio.php
@@ -10,7 +10,7 @@
    
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
         
-    <h2><?php the_title(); ?></h2>
+    <!-- <h2><?php the_title(); ?></h2> -->
   <?php the_content(); ?>
     <h3> <?php $author = get_the_author(); ?></h3>
       <?php endwhile; // end of the loop. ?>
@@ -21,6 +21,9 @@
             <img src="<?php echo $image['sizes']['large'] ?>">
     
       <?php endwhile; ?>
+      <div class="viewmore viewonline">
+      <a href=""><h1>View Project Online</h1></a>
+      </div>
     </div>
   </div> <!-- /.innerWrapper -->
 </div> <!-- /.section -->
